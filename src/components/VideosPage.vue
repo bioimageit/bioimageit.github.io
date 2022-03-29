ab8c076e611a42e6e429ab84399f8f8bc88bb6b2
PRIGENT Sylvain <sylvain.prigent@inria.fr>
2022-03-29 16:56:50 +0200

    update the video page

--- src/components/VideosPage.vue
+++ src/components/VideosPage.vue
@@ -3,33 +3,39 @@
   <site-navbar></site-navbar>
   <div class="container">
     <div class="text-center col-12">
-      <h2>BioimageIT installation Windows</h2>
+      <h2>BioImageIT With OMERO</h2>
        <iframe width="700" height="420"
-        src="https://www.youtube.com/embed/917InvFkivA">
-      </iframe> 
-    </div>
-    <div class="text-center col-12">
-      <h2>BioimageIT installation MacOS</h2>
-       <iframe width="700" height="420"
-        src="https://www.youtube.com/embed/jINaymkdl3s">
-      </iframe> 
-    </div>
-    <div class="text-center col-12">
-      <h2>BioimageIT installation Linux</h2>
-       <iframe width="700" height="420"
-        src="https://www.youtube.com/embed/ohKKkMb54k4">
+        src="https://www.youtube.com/embed/cDPn5OF9LRI">
       </iframe> 
     </div>
     <div class="text-center col-12">
       <h2>BioImpageIT tool demo</h2>
        <iframe width="700" height="420"
-        src="https://www.youtube.com/embed/cpN4dzASNu0">
+        src="https://www.youtube.com/embed/87JFlLHzsMU">
       </iframe> 
     </div>
     <div class="text-center col-12">
-      <h2>BioImpageIT data management demo</h2>
+      <h2>BioImageIT data management demo</h2>
        <iframe width="700" height="420"
-        src="https://www.youtube.com/embed/Ce0hLhO3Qis">
+        src="https://www.youtube.com/embed/PKXsEZ0fMxk">
+      </iframe> 
+    </div>
+    <div class="text-center col-12">
+      <h2>BioimageIT installation Windows</h2>
+       <iframe width="700" height="420"
+        src="https://www.youtube.com/embed/P8X28Z9HXaw">
+      </iframe> 
+    </div>
+    <div class="text-center col-12">
+      <h2>BioimageIT installation MacOS</h2>
+       <iframe width="700" height="420"
+        src="https://www.youtube.com/embed/R_dVrSbt1Ic">
+      </iframe> 
+    </div>
+    <div class="text-center col-12">
+      <h2>BioimageIT installation Linux</h2>
+       <iframe width="700" height="420"
+        src="https://www.youtube.com/embed/X3BJIfaIv14">
       </iframe> 
     </div>
 
