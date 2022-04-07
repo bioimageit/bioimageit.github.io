71553bfb8f8b4f46f4ef1ee1e06283e00021d70d
PRIGENT Sylvain <sylvain.prigent@inria.fr>
2022-04-07 16:15:22 +0200

    update site for 0.1.0 download

--- src/components/SiteNavbar.vue
+++ src/components/SiteNavbar.vue
@@ -7,17 +7,15 @@
     <b-collapse id="nav-collapse" is-nav>
       <b-navbar-nav> 
         <b-nav-item class="nav-item"><router-link class="nav-link bi-nav-item" to="/home">Home</router-link></b-nav-item>
-        <!-- <b-nav-item class="nav-item"><router-link class="nav-link bi-nav-item" to="/tools">What's inside</router-link></b-nav-item> -->
         <b-nav-item class="nav-item"><router-link class="nav-link bi-nav-item" to="/install">Install</router-link></b-nav-item>
         <b-nav-item class="nav-item"><router-link class="nav-link bi-nav-item" to="/documentation">Documentation</router-link></b-nav-item>
-        <!-- <b-nav-item class="nav-item"><router-link class="nav-link bi-nav-item" to="/tutorials">Tutorials</router-link></b-nav-item> -->
-        <!-- <b-nav-item class="nav-item"><router-link class="nav-link bi-nav-item" to="/developers">Developers</router-link></b-nav-item> -->
-        
-        
+
         <b-nav-item class="nav-item"><router-link class="nav-link bi-nav-item" to="/videos">Videos</router-link></b-nav-item>
         <b-nav-item class="nav-item"><router-link class="nav-link bi-nav-item" to="/events">Events</router-link></b-nav-item>
         <b-nav-item class="nav-item"><router-link class="nav-link bi-nav-item" to="/about">About</router-link></b-nav-item>
-        <!-- <b-nav-item class="nav-item"><router-link class="nav-link bi-nav-item" to="/mifobio">MiFoBio 2021</router-link></b-nav-item> -->
+        <a class="nav-link bi-nav-item-github" v-bind:href="'https://github.com/bioimageit/bioimageit'" target="_blank"><svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" viewBox="0 0 16 16">
+  <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z"/>
+</svg></a>
       </b-navbar-nav>
 
     </b-collapse>
@@ -62,6 +60,10 @@ export default {
   font-weight: bold;
 }
 .bi-nav-item {
+    text-transform: uppercase;
+}
+.bi-nav-item-github {
+    margin-top: 5px;
     text-transform: uppercase;
 }
 .bi-space {
