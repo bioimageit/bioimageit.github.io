4fe0bb1ffce5b737355cd3dc3c629a784c63bf83
Léo Maury <42140279+leomaury@users.noreply.github.com>
2022-07-29 11:28:58 +0200

    v0.1.2

--- src/components/InstallWindowsPage.vue
+++ src/components/InstallWindowsPage.vue
@@ -18,7 +18,7 @@
     <div class="bi-install">
       <h1>Windows install</h1>
 
-      <p>Download and execute the installer from <a href="https://github.com/bioimageit/bioimageit-install/raw/v0.1.1/windows/BioImageIT_install.exe" download>here</a></p>
+      <p>Download and execute the installer from <a href="https://github.com/bioimageit/bioimageit-install/raw/v0.1.2/windows/BioImageIT_install.exe" download>here</a></p>
       <p>When the install is finished, BioImageIT is installed in <i>C:\Users\username\BioimageIT\</i> and the BioImageIT 
       application should appear in the start menu</p>
     </div>
