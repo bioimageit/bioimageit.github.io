348213bd15e90b2f001c7f41d8e1ff6e4afe7ee0
leO9764 <leomaury97@gmail.com>
2022-11-14 15:45:09 +0100

    number new test

--- src/components/InstallPage.vue
+++ src/components/InstallPage.vue
@@ -7,7 +7,7 @@
         <h1><img src="~@/assets/logo.png" width="70"> <br />BioImageIT Install</h1>
         <br>
         <p>What is your operating system ?</p>
-        <p>(Downloaded {{file_download}} times)</p>
+        <p>(Downloaded {{file_download.value}} times)</p>
         </div>
     </div>
 
