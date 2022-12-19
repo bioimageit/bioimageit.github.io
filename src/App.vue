bf932fc797730e4eeb7d9d969de5fad148120a49
Léo Maury <42140279+leomaury@users.noreply.github.com>
2022-12-19 16:04:07 +0100

    SF Pro Display

--- src/App.vue
+++ src/App.vue
@@ -21,7 +21,7 @@ export default {
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: SF Pro Display, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #f0f1f2;
