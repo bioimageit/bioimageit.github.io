ad09b7650b1c703962898597cb0ab417905a73d5
Léo Maury <42140279+leomaury@users.noreply.github.com>
2022-12-20 11:46:23 +0100

    invert background and font colors

--- src/App.vue
+++ src/App.vue
@@ -24,10 +24,10 @@ export default {
   font-family: SF Pro Display, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  color: #f0f1f2;
+  color: #414851;
   margin-top: 0px;
   margin-left: 0px;
-  background-color: #414851;
+  background-color: #dce1e6;
   /*min-height: 2000px;*/
 }
 </style>
