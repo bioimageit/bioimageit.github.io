3046a2b995f41f66f0438f395d42289ddd2fbc08
Léo Maury <42140279+leomaury@users.noreply.github.com>
2023-02-13 16:39:45 +0100

    border-bottom-left-radius: 7px;

--- src/components/SiteNavbar.vue
+++ src/components/SiteNavbar.vue
@@ -57,6 +57,8 @@ export default {
   background-color: #5A626C;
   backdrop-filter: blur(20px);
   position: sticky;
+  border-bottom-left-radius: 7px;
+	border-bottom-right-radius: 7px;
 }
 
 .bi-brand {
