84f4f11fb53b3c013b01444b71d0b9972cedd7b0
Léo Maury <42140279+leomaury@users.noreply.github.com>
2023-02-15 17:15:56 +0100

    test

--- src/components/ToolboxNavbar.vue
+++ src/components/ToolboxNavbar.vue
@@ -27,5 +27,12 @@ export default {
   line-height: 24px;
   vertical-align: middle;
   padding-top: 9px;
+  
+  display: flex;
+  background-color: #5A626C;
+  backdrop-filter: blur(20px);
+  position: sticky;
+  border-bottom-left-radius: 10px;
+  border-bottom-right-radius: 10px;
 }
 </style>
