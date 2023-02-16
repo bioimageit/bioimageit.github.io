c5a415a41f5b6efcc3f85f6e8668643f084584a6
Léo Maury <42140279+leomaury@users.noreply.github.com>
2023-02-16 14:43:24 +0100

    shadow class

--- src/components/VideosPage.vue
+++ src/components/VideosPage.vue
@@ -4,49 +4,49 @@
   <div class="container">
     <div class="text-center col-12">
       <h2>BioImageIT using OMERO</h2>
-       <iframe width="700" height="420"
+       <iframe class="shadow" width="700" height="420"
         src="https://www.youtube.com/embed/cDPn5OF9LRI">
       </iframe>
     </div>
     <br>
     <div class="text-center col-12">
       <h2>BioImageIT tool demo (ND-SAFIR)</h2>
-       <iframe width="700" height="420"
+       <iframe class="shadow" width="700" height="420"
         src="https://www.youtube.com/embed/87JFlLHzsMU">
       </iframe> 
     </div>
     <br>
     <div class="text-center col-12">
       <h2>BioImageIT data management demo</h2>
-       <iframe width="700" height="420"
+       <iframe class="shadow" width="700" height="420"
         src="https://www.youtube.com/embed/PKXsEZ0fMxk">
       </iframe> 
     </div>
     <br>
     <div class="text-center col-12">
       <h2>Big data upload and real time visualization with OMERO</h2>
-       <iframe width="700" height="420"
+       <iframe class="shadow" width="700" height="420"
         src="https://www.youtube.com/embed/ywcojZ2YRTs">
       </iframe> 
     </div>
     <br>
     <div class="text-center col-12">
       <h2>BioImageIT installation Windows</h2>
-       <iframe width="700" height="420"
+       <iframe class="shadow" width="700" height="420"
         src="https://www.youtube.com/embed/Z0DP0tMZPEY">
       </iframe> 
     </div>
     <br>
     <div class="text-center col-12">
       <h2>BioImageIT installation MacOS</h2>
-       <iframe width="700" height="420"
+       <iframe class="shadow" width="700" height="420"
         src="https://www.youtube.com/embed/R_dVrSbt1Ic">
       </iframe> 
     </div>
     <br>
     <div class="text-center col-12">
       <h2>BioImageIT installation Linux</h2>
-       <iframe width="700" height="420"
+       <iframe class="shadow" width="700" height="420"
         src="https://www.youtube.com/embed/X3BJIfaIv14">
       </iframe> 
     </div>
@@ -74,6 +74,11 @@ export default {
 </script>
 
 <style scoped>
+.shadow{
+  box-shadow: 20px 20px 60px #343a40,
+              -20px -20px 60px #343a40;
+}
+
 .navbar-url {
     border: 1px solid #333333;
 }
